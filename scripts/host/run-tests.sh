70f1828b769201d5927defe2e58b2648b7b69faa
Per Thomas <pth@embc.no>
2020-12-27 18:28:06 +0100

    jenkins

--- scripts/host/run-tests.sh
+++ scripts/host/run-tests.sh
@@ -2,6 +2,8 @@
 
 image_name=cpp-cross-compiler
 
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu/
+
 if [ $1 = "x86" ] || [ $1 = "arm" ] || [ $1 = "clean" ]; then
     docker run --user root --rm -v ${PWD}:/home/root/ --workdir /home/root/ $image_name "./scripts/docker/run-unittests.sh" $1 $2 $3 $4 
     exit $?;    
